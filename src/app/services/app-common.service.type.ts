3f10bda17811b415f08a648c4af733857f94de49
Huda Prasetyo <44672728+hudaprs@users.noreply.github.com>
2023-06-06 19:48:02 +0700

    feat(pagination): improve pagination (#10)
    * feat(auth): preparing
    
    * feat(auth): adding google SMTP
    
    * feat(auth): preparing SMTP for verification
    
    * feat(auth): completing
    
    * feat(pagination): improve pagination

--- src/app/services/app-common.service.type.ts
+++ src/app/services/app-common.service.type.ts
@@ -1,14 +1,41 @@
 // Prisma
 import type { Prisma } from '@prisma/client'
 
-// Express
-import { Request } from 'express'
-
 export type TPrismaPaginateArgs = Prisma.RoleFindManyArgs & {
-	page?: number | undefined
-	limit?: number | undefined
+	params: {
+		page?: number | undefined
+		limit?: number | undefined
+		sort?: string | undefined
+		column?: string | undefined
+	}
+	prisma: {
+		take: any
+		skip: any
+		orderBy: any
+	}
+}
+
+export type TPaginationArgsPrisma = {
+	orderBy?: any | undefined
+}
+
+export type TPaginationArgs<T extends TPaginationArgsPrisma> = T & {
+	take?: number | undefined
+	skip?: number | undefined
+	orderBy?: T['orderBy'] | undefined
+	page?: string | undefined
+	limit?: string | undefined
 	sort?: string | undefined
 	column?: string | undefined
+}
+
+export type TPagination<T> = {
+	limit: number
+	totalPages: number
+	totalRows: number
+	page: number
+	rows: T[]
+	sort: string
 }
 
 export type TPrismaPaginateResponse<T = unknown> = {
@@ -20,12 +47,14 @@ export type TPrismaPaginateResponse<T = unknown> = {
 	rows: T[]
 }
 
+export type TPaginateResult<T> = { result: T[]; total: number }
+
 export type TAppCommonService = {
-	parsePaginationArgs: (query: Request['query']) => TPrismaPaginateArgs
 	generateOtp: () => string
+	paginateArgs: <T extends TPaginationArgsPrisma>(args: TPaginationArgs<T>) => T
 	paginate: <T>(
-		model: any,
-		args?: TPrismaPaginateArgs
-	) => Promise<TPrismaPaginateResponse<T>>
+		result: TPaginateResult<T>,
+		args: TPaginationArgs<TPaginationArgsPrisma>
+	) => TPagination<T>
 	hashPassword: (password: string) => Promise<string>
 }
